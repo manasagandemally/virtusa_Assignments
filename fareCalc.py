ca91e825acc31180cb77e6e5a4503acf47b39ea3
manasagandemally <gandemallymanasa@gmail.com>
2026-04-24 10:15:30 +0530

    updated code

--- fareCalc.py
+++ fareCalc.py
@@ -1,13 +1,8 @@
 # The "Fare Calc" Travel Optimizer
 
-def calculate_fare(km, type, hour):
-    rates= {
-        'ECONOMY': 10,
-        'PREMIUM': 18,
-        'SUV': 25
-    }
+def calculate_fare(km, veh_type, hour):
     
-    fare= km * rates[type]
+    fare= km * rates[veh_type]
     if 17<=hour<=20:
         multiplier=1.5
     else:
@@ -18,27 +13,27 @@ def calculate_fare(km, type, hour):
 
 try:
     km= float(input("Enter distance in km: "))
-    type= input("choose a vehicle type (economy/Premium/SUV): ").upper()
-    rates={'ECONOMY', 'PREMIUM', 'SUV'}
-    if type not in rates:
+    veh_type= input("choose a vehicle type (economy/Premium/SUV): ").upper()
+    rates={'ECONOMY': 10, 'PREMIUM': 18, 'SUV': 25}
+    if veh_type not in rates:
         print("Vehicle service not available.")
         exit()
     hour= int(input("enter hour of travel (0-23): "))
-    result= calculate_fare(km, type, hour)
-    print("Total fare of travel: ")
-    if result is None:
-        print(result)
+    if not (0 <= hour <= 23):
+        print("Hour must be between 0 and 23.")
+        exit()
+
+    result= calculate_fare(km, veh_type, hour)
+    
+    print("Distance:", km, "km")
+    print("vehicle type:", veh_type)
+    print("hour:", hour)
+
+    if(17<=hour<=20):
+        print("Surge Applied : YES (1.5x)")
     else:
-        print("Distance:", km, "km")
-        print("vehicle type:", type)
-        print("hour:", hour)
+        print("NO Surge Applied")
 
-        if(17<=hour<=20):
-            print("Surge Applied : YES (1.5x)")
-        else:
-            print("NO Surge Applied")
-
-        print("Total Fare: ", round(result,2))
-
+    print("Total Fare: ", round(result,2))
 except:
     print("Invalid Input")
